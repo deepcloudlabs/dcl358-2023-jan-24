3c2280a208fa0250ed5261a754e17268fb7c0580
Binnur KURT <deepcloudlabs@gmail.com>
2023-01-25 18:19:25 +0300

    initial commit

--- hr-consumer/src/main/java/com/example/listener/HrEventListenerService.java
+++ hr-consumer/src/main/java/com/example/listener/HrEventListenerService.java
@@ -1,5 +1,6 @@
 package com.example.listener;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,9 +17,18 @@ public class HrEventListenerService {
 	}
 
 	@KafkaListener(topics = "${topicName}", groupId = "${spring.application.name}")
-	public void listenHrEvent(String eventAsString) throws Exception {
+	public void listenKafkaHrEvent(String eventAsString) throws Exception {
 		var hrEvent = objectMapper.readValue(eventAsString, HrEventDTO.class);
 		System.err.println("New hr event has arrived from kafka: %s.".formatted(hrEvent));
+		var restTemplate = new RestTemplate();
+		var employee = restTemplate.getForEntity("http://localhost:4001/hr/api/v1/employees/%s".formatted(hrEvent.getIdentityNo().getValue()), String.class).getBody();
+		System.out.println(employee);
+	}
+	
+	@RabbitListener(queues = "${queueName}")
+	public void listenRabbitHrEvent(String eventAsString) throws Exception {
+		var hrEvent = objectMapper.readValue(eventAsString, HrEventDTO.class);
+		System.err.println("New hr event has arrived from rabbit: %s.".formatted(hrEvent));
 		var restTemplate = new RestTemplate();
 		var employee = restTemplate.getForEntity("http://localhost:4001/hr/api/v1/employees/%s".formatted(hrEvent.getIdentityNo().getValue()), String.class).getBody();
 		System.out.println(employee);
---
+++ hr-consumer/src/main/java/com/example/listener/WebsocketClientService.java
@@ -0,0 +1,55 @@
+package com.example.listener;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.WebSocketClient;
+
+@Service
+public class WebsocketClientService implements WebSocketHandler {
+	private final WebSocketClient webSocketClient;
+	private final String hrMicroserviceUrl;
+
+	public WebsocketClientService(@Value("${hrMicroserviceUrl}") String hrMicroserviceUrl, 
+			WebSocketClient webSocketClient) {
+		this.hrMicroserviceUrl = hrMicroserviceUrl;
+		this.webSocketClient = webSocketClient;
+	}
+
+	@PostConstruct
+	public void connect() {
+		webSocketClient.doHandshake(this, hrMicroserviceUrl);
+	}
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.err.println("Connected to the binance ws server.");
+	}
+
+	@Override
+	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+		var eventAsString = message.getPayload().toString();
+		System.err.println("New event has arrived through websocket: %s".formatted(eventAsString));
+	}
+
+	@Override
+	public void handleTransportError(WebSocketSession session, Throwable e) throws Exception {
+		System.err.println("An error has occured: %s.".formatted(e.getMessage()));
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+		System.err.println("Disconnected from the binance ws server.");
+	}
+
+	@Override
+	public boolean supportsPartialMessages() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
